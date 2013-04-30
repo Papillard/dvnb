610f5808557bb5b7cd42cefaa917035ab1e09610
Boris Paillard <boris.paillard@gmail.com>
2013-04-30 17:41:09 +0200

    adding users

--- config/environments.rb
+++ config/environments.rb
@@ -1,14 +1,9 @@
-#The environment variable DATABASE_URL should be in the following format:
-# => postgres://{user}:{password}@{host}:{port}/path
-configure :production, :development do
-	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
- 
+configure :production, :development do 
 	ActiveRecord::Base.establish_connection(
-			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-			:host     => db.host,
-			:username => db.user,
-			:password => db.password,
-			:database => db.path[1..-1],
-			:encoding => 'utf8'
+			:adapter => 'postgresql',
+			:username => 'postgres',
+			:database => 'dnb_prod',
+			:encoding => 'unicode'
 	)
 end
+
