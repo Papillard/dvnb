c990cab1188deb45f489dfcc60b96f0be9d77fb4
Boris Paillard <boris.paillard@gmail.com>
2013-04-30 17:35:47 +0200

    adding users

--- app.rb
+++ app.rb
@@ -12,6 +12,12 @@ get "/" do
   erb :landing_page
 end
 
+post "/" do 
+  User.create(email: params[:email])
+  @notice_title = "Merci, #{params[:email]}"
+  @notice_desc = "Nous te contacterons pour le prochain D&B !"
+  erb :landing_page
+end
 
 helpers do
   # No helpers for the moment !
