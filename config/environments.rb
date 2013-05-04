5ffa723a2ea2b85690c30120cae506f99a63fb77
mathieuripert <mathieu.ripert@gmail.com>
2013-05-04 11:20:31 +0200

    ripert

--- config/environments.rb
+++
@@ -1,9 +0,0 @@
-configure :production, :development do 
-	ActiveRecord::Base.establish_connection(
-			:adapter => 'postgresql',
-			:username => 'postgres',
-			:database => 'dnb_prod',
-			:encoding => 'unicode'
-	)
-end
-
